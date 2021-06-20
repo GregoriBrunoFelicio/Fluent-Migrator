be47e99f0b31502d8851aa7b1c629c22b3c0ea69
gregori bruno felicio <gregory.wow@hotmail.com>
2021-06-20 01:08:32 -0300

    create migration to console

---
+++ FluentMigrator.Console/Migrations/01CreateUserTable.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentMigrator.Console.Migrations
+{
+    [Migration(01)]
+    public class CreateUserTable : Migration
+    {
+        private const string tableName = "User";
+
+        public override void Up() =>
+            Create.Table(tableName)
+                .WithColumn("Id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("Name").AsAnsiString().NotNullable()
+                .WithColumn("Date").AsDateTime().Nullable();
+
+        public override void Down() => Delete.Table(tableName);
+    }
+}
---
+++ FluentMigrator.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using FluentMigrator.Console.Migrations;
+
+namespace FluentMigrator.Console
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var serviceProvider = CreateServices();
+
+            using var scope = serviceProvider.CreateScope();
+            UpdateDatabase(scope.ServiceProvider);
+        }
+
+        private static IServiceProvider CreateServices() =>
+            new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(
+                    rb => rb
+                        .AddSqlServer()
+                        .WithGlobalConnectionString(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=User;Integrated Security=True;")
+                        .ScanIn(typeof(CreateUserTable).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider();
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
